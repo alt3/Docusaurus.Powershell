2c6ffed14cde1042f0d8926c4dbc08a4acc468f3
bravo-kernel <bravo-kernel@users.noreply.github.com>
2019-10-22 15:27:05 +0200

    chore: update docusaurus description meta tag

--- docusaurus/src/pages/index.js
+++ docusaurus/src/pages/index.js
@@ -49,7 +49,7 @@ function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Awesome documentation for Powershell Modules<head />">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
