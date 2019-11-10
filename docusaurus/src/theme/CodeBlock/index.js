c7bcb595d205b1e229a999c998e6ae87b482e4e5
bravo-kernel <bravo-kernel@users.noreply.github.com>
2019-11-10 10:32:29 +0100

    chore(site): bumps docusaurus to v2.0.0-alpha.33 (default language and line highlighting)

--- docusaurus/src/theme/CodeBlock/index.js
+++ docusaurus/src/theme/CodeBlock/index.js
@@ -4,9 +4,8 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * FYI this is a swizzled theme component so we can apply syntax
- * highlighting to powershell code blocks as explaeind at
- * https://v2.docusaurus.io/docs/markdown-features#syntax-highlighting.
+ * Code block swizzled by running this command inside the `docusaurus` folder:
+ * yarn docusaurus swizzle @docusaurus/theme-classic CodeBlock
  */
 
 import React, {useEffect, useState, useRef} from 'react';
@@ -14,6 +13,7 @@ import classnames from 'classnames';
 import Highlight, {defaultProps} from 'prism-react-renderer';
 import defaultTheme from 'prism-react-renderer/themes/palenight';
 import Clipboard from 'clipboard';
+import rangeParser from 'parse-numeric-range';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './styles.module.css';
 
@@ -21,15 +21,23 @@ import Prism from 'prism-react-renderer/prism';
 (typeof global !== 'undefined' ? global : window).Prism = Prism;
 require('prismjs/components/prism-powershell');
 
-export default ({children, className: languageClassName}) => {
+const highlightLinesRangeRegex = /{([\d,-]+)}/;
+
+export default ({children, className: languageClassName, metastring}) => {
   const {
     siteConfig: {
-      themeConfig: {prismTheme},
+      themeConfig: {prism = {}},
     },
   } = useDocusaurusContext();
   const [showCopied, setShowCopied] = useState(false);
   const target = useRef(null);
   const button = useRef(null);
+  let highlightLines = [];
+
+  if (metastring && highlightLinesRangeRegex.test(metastring)) {
+    const highlightLinesRange = metastring.match(highlightLinesRangeRegex)[1];
+    highlightLines = rangeParser.parse(highlightLinesRange).filter(n => n > 0);
+  }
 
   useEffect(() => {
     let clipboard;
@@ -47,8 +55,12 @@ export default ({children, className: languageClassName}) => {
     };
   }, [button.current, target.current]);
 
-  const language =
+  let language =
     languageClassName && languageClassName.replace(/language-/, '');
+
+  if (!language && prism.defaultLanguage) {
+    language = prism.defaultLanguage;
+  }
 
   const handleCopyCode = () => {
     window.getSelection().empty();
@@ -60,7 +72,7 @@ export default ({children, className: languageClassName}) => {
   return (
     <Highlight
       {...defaultProps}
-      theme={prismTheme || defaultTheme}
+      theme={prism.theme || defaultTheme}
       code={children.trim()}
       language={language}>
       {({className, style, tokens, getLineProps, getTokenProps}) => (
@@ -69,13 +81,21 @@ export default ({children, className: languageClassName}) => {
             ref={target}
             className={classnames(className, styles.codeBlock)}
             style={style}>
-            {tokens.map((line, i) => (
-              <div key={i} {...getLineProps({line, key: i})}>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({token, key})} />
-                ))}
-              </div>
-            ))}
+            {tokens.map((line, i) => {
+              const lineProps = getLineProps({line, key: i});
+
+              if (highlightLines.includes(i + 1)) {
+                lineProps.className = `${lineProps.className} docusaurus-highlight-code-line`;
+              }
+
+              return (
+                <div key={i} {...lineProps}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({token, key})} />
+                  ))}
+                </div>
+              );
+            })}
           </pre>
           <button
             ref={button}
