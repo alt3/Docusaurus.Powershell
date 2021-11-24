4b8c2cd75881f6b3b16fffd4aadcce608af40626
bravo-kernel <bravo-kernel@users.noreply.github.com>
2021-11-24 19:50:50 +0100

    Fix parameters

--- .github/actions/psgallery-publisher/action.ps1
+++ .github/actions/psgallery-publisher/action.ps1
@@ -2,10 +2,13 @@
     .SYNOPSIS
     Publishes a module to the Powershell Gallery.
 #>
+[cmdletbinding()]
+Param(
+    [Parameter(Mandatory=$true)][string]$ModulePath
+)
+
 Set-StrictMode -Version Latest
 $PSDefaultParameterValues['*:ErrorAction'] = "Stop" # full script stop on first error
-
-Write-Host "Publishing modulePath = $($env:INPUT_MODULEPATH)"
 
 # Make sure the secret key PSGALLERY_API_KEY is set
 if (-not $env:PSGALLERY_API_KEY) {
@@ -13,5 +16,5 @@ if (-not $env:PSGALLERY_API_KEY) {
 }
 
 # Publish the module
-Write-Host "Publishing modulePath = $($env:INPUT_MODULEPATH)"
-Publish-Module -Path $env:INPUT_MODULEPATH -NuGetApiKey $env:PSGALLERY_API_KEY -Verbose -WhatIf
+Write-Host "Publishing modulePath = $ModulePath"
+Publish-Module -Path $ModulePath -NuGetApiKey $env:PSGALLERY_API_KEY -Verbose -WhatIf
