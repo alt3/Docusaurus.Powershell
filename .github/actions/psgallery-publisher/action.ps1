1a6fe20a76465c9e6cb4da42bc27e8dce5d103f3
bravo-kernel <bravo-kernel@users.noreply.github.com>
2021-11-24 20:43:05 +0100

    detect version

--- .github/actions/psgallery-publisher/action.ps1
+++ .github/actions/psgallery-publisher/action.ps1
@@ -12,9 +12,14 @@ $PSDefaultParameterValues['*:ErrorAction'] = "Stop" # full script stop on first 
 
 # Make sure the secret key PSGALLERY_API_KEY is set
 if (-not $env:PSGALLERY_API_KEY) {
-    Throw "Secret key PSGALLERY_API_KEY has not been set"
+    Throw "Github secret PSGALLERY_API_KEY has not been set"
 }
+
+# Find path to the module version
+Write-Host "Module path = $ModulePath"
+$moduleVersion = (Get-ChildItem $ModulePath -Directory | Sort-Object | Select-Object -Last 1).Name
+Write-Host "Module version = $moduleVersion"
 
 # Publish the module
 Write-Host "Publishing modulePath = $ModulePath"
-Publish-Module -Path $ModulePath -NuGetApiKey $env:PSGALLERY_API_KEY -Verbose -WhatIf
+Publish-Module -Path (Join-Path -Path $ModulePath -ChildPath $moduleVersion) -NuGetApiKey $env:PSGALLERY_API_KEY -Verbose -WhatIf
