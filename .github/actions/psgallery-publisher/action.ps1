e9ad2884fb548a2ecc22263157a0ee4a6f631cfe
bravo-kernel <bravo-kernel@users.noreply.github.com>
2021-11-24 19:21:11 +0100

    fix secret

--- .github/actions/psgallery-publisher/action.ps1
+++ .github/actions/psgallery-publisher/action.ps1
@@ -5,11 +5,13 @@
 Set-StrictMode -Version Latest
 $PSDefaultParameterValues['*:ErrorAction'] = "Stop" # full script stop on first error
 
+Write-Host "Publishing modulePath = $($env:INPUT_MODULEPATH)"
+
 # Make sure the secret key PSGALLERY_API_KEY is set
-if (-not $env:INPUT_APIKEY) {
+if (-not $env:PSGALLERY_API_KEY) {
     Throw "Secret key PSGALLERY_API_KEY has not been set"
 }
 
 # Publish the module
 Write-Host "Publishing modulePath = $($env:INPUT_MODULEPATH)"
-Publish-Module -Path $env:INPUT_MODULEPATH -NuGetApiKey $env:INPUT_APIKEY -Verbose -WhatIf
+Publish-Module -Path $env:INPUT_MODULEPATH -NuGetApiKey $env:PSGALLERY_API_KEY -Verbose -WhatIf
