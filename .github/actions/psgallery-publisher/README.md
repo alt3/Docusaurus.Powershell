fce417297b8e107f727bfc2b0cc5bec581137e28
bravo-kernel <bravo-kernel@users.noreply.github.com>
2021-11-24 19:11:16 +0100

    Fix variables

---
+++ .github/actions/psgallery-publisher/README.md
@@ -0,0 +1,4 @@
+# PSGallery Publisher
+
+Github Action for publishing a Powershell module (artifact) to the PSGallery.
+
--- .github/actions/psgallery-publisher/action.ps1
+++ .github/actions/psgallery-publisher/action.ps1
@@ -2,18 +2,14 @@
     .SYNOPSIS
     Publishes a module to the Powershell Gallery.
 #>
-[cmdletbinding()]
-Param(
-    [string]$ModulePath
-)
 Set-StrictMode -Version Latest
 $PSDefaultParameterValues['*:ErrorAction'] = "Stop" # full script stop on first error
 
 # Make sure the secret key PSGALLERY_API_KEY is set
-if (-not $env:PSGALLERY_API_KEY) {
+if (-not $env:INPUT_APIKEY) {
     Throw "Secret key PSGALLERY_API_KEY has not been set"
 }
 
 # Publish the module
-Write-Host "ModulePath = $ModulePath"
-Publish-Module -Path $ModulePath -NuGetApiKey $env:PSGALLERY_API_KEY -Verbose -WhatIf
+Write-Host "Publishing modulePath = $($env:INPUT_MODULEPATH)"
+Publish-Module -Path $env:INPUT_MODULEPATH -NuGetApiKey $env:INPUT_APIKEY -Verbose -WhatIf
