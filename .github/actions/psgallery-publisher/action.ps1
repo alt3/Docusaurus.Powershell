3d9f064234cf7f2207eebaf6c8f7008b670401b8
bravo-kernel <bravo-kernel@users.noreply.github.com>
2021-11-25 12:56:42 +0100

    write-output

--- .github/actions/psgallery-publisher/action.ps1
+++ .github/actions/psgallery-publisher/action.ps1
@@ -10,32 +10,18 @@ Param(
 Set-StrictMode -Version Latest
 $PSDefaultParameterValues['*:ErrorAction'] = "Stop" # full script stop on first error
 
-# Debug current directory
-Get-ChildItem
-
-# Debug the module path
-Write-Host "D:"
-Get-ChildItem -Path "D:"
-
-Write-Host "D:\a"
-Get-ChildItem -Path "D:\a"
-
-Write-Host "D:\a\Docusaurus.Powershell:"
-Get-ChildItem -Path "D:\a\Docusaurus.Powershell"
-
-Write-Host "D:\a\Docusaurus.Powershell\Docusaurus.Powershell:"
-Get-ChildItem -Path "D:\a\Docusaurus.Powershell\Docusaurus.Powershell"
-
 # Make sure the secret key PSGALLERY_API_KEY is set
 if (-not $env:PSGALLERY_API_KEY) {
     Throw "Github secret PSGALLERY_API_KEY has not been set"
 }
 
-# Find path to the module version
-Write-Host "Module path = $ModulePath"
+# Construct path to the module version inside the artifact folder
+Write-Output "Module path    = $ModulePath"
 $moduleVersion = (Get-ChildItem $ModulePath -Directory | Sort-Object | Select-Object -Last 1).Name
-Write-Host "Module version = $moduleVersion"
+Write-Output "Module version = $moduleVersion"
+$fullPath = Join-Path -Path $ModulePath -ChildPath $moduleVersion
+Write-Output "Full path      = $fullPath"
 
 # Publish the module
-Write-Host "Publishing modulePath = $ModulePath"
-Publish-Module -Path (Join-Path -Path $ModulePath -ChildPath $moduleVersion) -NuGetApiKey $env:PSGALLERY_API_KEY -Verbose -WhatIf
+Write-Output "Publish the module:"
+Publish-Module -Path $fullPath -NuGetApiKey $env:PSGALLERY_API_KEY -Verbose -WhatIf
