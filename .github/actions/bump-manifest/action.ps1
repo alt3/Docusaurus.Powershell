2c8126e043c46e55d430abbe7d16e57d65c11bc9
bravo-kernel <bravo-kernel@users.noreply.github.com>
2021-11-26 13:56:01 +0100

    re-run

--- .github/actions/bump-manifest/action.ps1
+++ .github/actions/bump-manifest/action.ps1
@@ -1,34 +1,60 @@
 <#
     .SYNOPSIS
     Replaces module manifest on master with bumped manifest in the artifact, then creates the commit.
+
+    .PARAMETER ArtifactFolder
+    Path to the artifact holding the newly built module (e.g. Modules/Alt3.Docusaurus.Powershell)
+
+    .PARAMETER MasterFolder
+    Path to the master repo (e.g. __master)
 #>
 [cmdletbinding()]
 Param(
-    [Parameter(Mandatory=$true)][string]$ArtifactPath,
-    [Parameter(Mandatory=$true)][string]$Module
+    [Parameter(Mandatory=$true)][string]$ArtifactFolder,
+    [Parameter(Mandatory=$true)][string]$MasterFolder
 )
+
+# construct the paths
+Write-Output "================================================================================"
+Write-Output "➡ artifact folder   = $ArtifactFolder"
+Write-Output "➡ master folder     = $MasterFolder"
+
+$moduleName = (Get-Item -Path $ArtifactFolder).Name
+Write-Output "➡ module name       = $moduleName"
+
+$artifactVersion = (Get-ChildItem $ArtifactFolder -Directory | Sort-Object | Select-Object -Last 1).Name
+Write-Output "➡ artifact version  = $moduleName"
+
+$artifactManifestPath = Join-Path $ArtifactFolder -ChildPath $artifactVersion | Join-Path -ChildPath "$moduleName.psd1"
+Write-Output "➡ artifact manifest = $artifactManifestPath"
+
+$masterManifestPath = Join-Path $MasterFolder -ChildPath "Source" | Join-Path -ChildPath "$moduleName.psd1"
+Write-Output "♥ master manifest   = $masterManifestPath"
 
 # some debugging
 
-Get-ChildItem
+# Get-ChildItem
+# Get-ChildItem Modules
 
-git branch -a
 
-Write-Output "================================================================================"
-Write-Output "PSD1 on master:"
-Write-Output "=> module          = $Module"
-$masterManifest = Join-Path -Path "Source" -ChildPath "$Module.psd1"
-Write-Output "=> master manifest = $masterManifest"
-Get-Content $masterManifest
 
-Write-Output "================================================================================"
-Write-Output "PSD1 in artifact:"
-Write-Output "=> artifact path    = $ArtifactPath"
-$artifactVersion = (Get-ChildItem $ArtifactPath -Directory | Sort-Object | Select-Object -Last 1).Name
-Write-Output "=> artifact version = $artifactVersion"
-$artifactManifest = Join-Path $ArtifactPath -ChildPath $artifactVersion | Join-Path -ChildPath "$Module.psd1"
-Write-Output "=> artifact manifest = $artifactManifest"
+# git branch -a
 
-Get-Content $artifactManifest
+# Write-Output "================================================================================"
+# Write-Output "PSD1 on master:"
+# Write-Output "=> module          = $Module"
+# $masterManifest = Join-Path -Path "Source" -ChildPath "$Module.psd1"
+# Write-Output "=> master manifest = $masterManifest"
+# Get-Content $masterManifest
+
+# Write-Output "================================================================================"
+# Write-Output "PSD1 in artifact:"
+# Write-Output "=> artifact path    = $ArtifactFolder"
+# $artifactVersion = (Get-ChildItem $ArtifactFolder -Directory | Sort-Object | Select-Object -Last 1).Name
+# Write-Output "=> artifact version = $artifactVersion"
+# $artifactManifest = Join-Path $ArtifactFolder -ChildPath $artifactVersion | Join-Path -ChildPath "$Module.psd1"
+# Write-Output "=> artifact manifest = $artifactManifest"
+
+# Get-Content $artifactManifest
 
 
