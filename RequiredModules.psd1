d577768468a9f1c22c6d81761a596ca2916eaf62
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-06-24 17:41:15 +0200

    First run

--- RequiredModules.psd1
+++ RequiredModules.psd1
@@ -1,6 +1,6 @@
 @{
     ModuleBuilder    = "2.*"
-    Pester           = "5.3.3"
+    Pester           = "5.3.3" # pinned until poshcode GA handles wildcards
     PSScriptAnalyzer = "1.*"
     PlatyPS          = "0.*"
 }
--- Tests/Unit/Private/GetCustomEditUrl.Tests.ps1
+++ Tests/Unit/Private/GetCustomEditUrl.Tests.ps1
@@ -1,6 +1,7 @@
 BeforeAll {
-    # dot source the function
-    . $PSScriptRoot/../../../Source/Private/GetCustomEditUrl.ps1
+    # if (-not(Get-Module Alt3.Docusaurus.PowerShell)) {
+    #     Import-Module Alt3.Docusaurus.PowerShell -DisableNameChecking -Verbose:$True -Scope Global
+    # }
 
     # create dummy markdown file
     $markdownFilePath = Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath 'Dummy-PesterCommand.md'
@@ -24,31 +25,33 @@ Describe "Private$([IO.Path]::DirectorySeparatorChar)GetCustomEditUrl" {
     }
 
     It "Returns `$null when omitting optional argument -EditUrl" {
-        GetCustomEditUrl -Module "DummyModule" -MarkdownFile $markdownFile | Should -BeNullOrEmpty
-    }
-
-    It "Returns string ""null"" when using -EditUrl ""null""" {
-        GetCustomEditUrl -Module "DummyModule" -MarkdownFile $markdownFile -EditUrl "null" | Should -Be "null"
-    }
-
-    Context "for non-monolithic modules" {
-        It "Will always use markdown file name to generate URL pointing to the correlating .ps1 (function) source file when using -EditUrl ""https://site.com""" {
-            GetCustomEditUrl -Module "DummyModule" -MarkdownFile $markdownFile -EditUrl "https://site.com" |
-            Should -Be "https://site.com/Dummy-PesterCommand.ps1"
+        InModuleScope Alt3.Docusaurus.Powershell -Parameters @{MarkdownFile = $markdownFile } {
+            GetCustomEditUrl -Module "DummyModule" -MarkdownFile $MarkdownFile | Should -BeNullOrEmpty
         }
     }
 
-    Context "for monolithic repos" {
-        It "Will first try to use markdown file name to generate URL pointing to the correlating .psm1 (module) source file when using -EditUrl ""https://site.com"" -Monolithic" {
-            GetCustomEditUrl -Module $module -MarkdownFile $markdownFile -EditUrl "https://site.com" -Monolithic |
-            Should -Be "https://site.com/DummyModule.psm1"
-        }
+    # It "Returns string ""null"" when using -EditUrl ""null""" {
+    #     GetCustomEditUrl -Module "DummyModule" -MarkdownFile $markdownFile -EditUrl "null" | Should -Be "null"
+    # }
 
-        It "Will otherwise simply use passed module name to generate URL pointing to the correlating .psm1 (module) source file when using -EditUrl ""https://site.com"" -Monolithic" {
-            GetCustomEditUrl -Module Microsoft.PowerShell.Management -MarkdownFile $markdownFile -EditUrl "https://site.com" -Monolithic |
-            Should -Be "https://site.com/Microsoft.PowerShell.Management.psm1"
-        }
-    }
+    # Context "for non-monolithic modules" {
+    #     It "Will always use markdown file name to generate URL pointing to the correlating .ps1 (function) source file when using -EditUrl ""https://site.com""" {
+    #         GetCustomEditUrl -Module "DummyModule" -MarkdownFile $markdownFile -EditUrl "https://site.com" |
+    #         Should -Be "https://site.com/Dummy-PesterCommand.ps1"
+    #     }
+    # }
+
+    # Context "for monolithic repos" {
+    #     It "Will first try to use markdown file name to generate URL pointing to the correlating .psm1 (module) source file when using -EditUrl ""https://site.com"" -Monolithic" {
+    #         GetCustomEditUrl -Module $module -MarkdownFile $markdownFile -EditUrl "https://site.com" -Monolithic |
+    #         Should -Be "https://site.com/DummyModule.psm1"
+    #     }
+
+    #     It "Will otherwise simply use passed module name to generate URL pointing to the correlating .psm1 (module) source file when using -EditUrl ""https://site.com"" -Monolithic" {
+    #         GetCustomEditUrl -Module Microsoft.PowerShell.Management -MarkdownFile $markdownFile -EditUrl "https://site.com" -Monolithic |
+    #         Should -Be "https://site.com/Microsoft.PowerShell.Management.psm1"
+    #     }
+    # }
 }
 
 AfterAll {
