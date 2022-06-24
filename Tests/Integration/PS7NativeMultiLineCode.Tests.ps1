49b49273b59f627ce66a0f5da5dfa5ae9adb30ee
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-06-24 19:38:32 +0200

    Upgrade PS7NativeMultiLineCode.Tests.ps1

--- Tests/Integration/PS7NativeMultiLineCode.Tests.ps1
+++ Tests/Integration/PS7NativeMultiLineCode.Tests.ps1
@@ -4,55 +4,48 @@
         This test ensures PowerShell 7 native multi-line code examples render as expected.
 #>
 
-# -----------------------------------------------------------------------------
-# skip test on PowerShell 6 and lower
-# -----------------------------------------------------------------------------
-if ($PSVersionTable.PSVersion.Major -lt 7) {
-    Write-Host "[+] skipping test because PowerShell 6 or lower" -ForegroundColor magenta
-    return
-}
+BeforeAll {
+    [bool]$isPS7 = $PSVersionTable.PSVersion.Major -ge 7
 
-# -----------------------------------------------------------------------------
-# import the Alt3.Docusaurus.PowerShell rendering module
-# -----------------------------------------------------------------------------
-if (-not(Get-Module Alt3.Docusaurus.PowerShell)) {
-    Import-Module Alt3.Docusaurus.PowerShell -DisableNameChecking -Verbose:$False -Scope Global
-}
+    write-Host "isPS7 = $isPS7"
 
-# -----------------------------------------------------------------------------
-# import the test module associated with this test
-# -----------------------------------------------------------------------------
-${global:testModuleName} = [regex]::replace([System.IO.Path]::GetFileName($PSCommandPath), '.Tests.ps1', '')
-${global:testModulePath} = Join-Path -Path $PSScriptRoot -ChildPath "${global:testModuleName}.psm1"
-Import-Module ${global:testModulePath} -Force -DisableNameChecking -Verbose:$False -Scope Global
-
-# -----------------------------------------------------------------------------
-# the actual integration test
-# -----------------------------------------------------------------------------
-Describe "Integration Test to ensure PowerShell 7's Native Multi-Line Code Examples render as expected" {
-
-    # render the markdown
-    ${global:DocsFolder} = Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath ${global:testModuleName}
-    InModuleScope Alt3.Docusaurus.PowerShell {
-        New-DocusaurusHelp -Module ${global:testModulePath} -DocsFolder ${global:DocsFolder}
+    if (-not(Get-Module Alt3.Docusaurus.PowerShell)) {
+        throw "Required module 'Alt3.Docusaurus.Powershell' is not loaded."
     }
 
-    # read markdown
-    $renderedMdxFile = Join-Path -Path ${global:DocsFolder} -ChildPath "commands" | Join-Path -ChildPath "Test-$(${global:testModuleName}).mdx"
-    $renderedMdx = Get-Content $renderedMdxFile
-    $expectedMdx = Get-Content (Join-Path -Path $PSScriptRoot -ChildPath "$(${global:testModuleName}).expected.mdx")
+    # import the PS7NativeMultiLineCode.psm1 test module
+    $testModuleName = [regex]::replace([System.IO.Path]::GetFileName($PSCommandPath), '.Tests.ps1', '')
+    $testModulePath = Join-Path -Path $PSScriptRoot -ChildPath "$testModuleName.psm1"
+    Import-Module $testModulePath -Force -DisableNameChecking -Verbose:$False -Scope Global
 
+    # generate Docusaurus docs folder for the module
+    $docsFolder = Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath $testModuleName
+    InModuleScope Alt3.Docusaurus.PowerShell -Parameters @{TestModulePath = $testModulePath; DocsFolder = $docsFolder }{
+        New-DocusaurusHelp -Module $testModulePath -DocsFolder $DocsFolder
+    }
+
+    # handle generated .mdx file
+    $renderedMdxFile = Join-Path -Path $docsFolder -ChildPath "commands" | Join-Path -ChildPath "Test-$testModuleName.mdx"
+    $renderedMdx = Get-Content $renderedMdxFile
+    $expectedMdx = Get-Content (Join-Path -Path $PSScriptRoot -ChildPath "$testModuleName.expected.mdx")
+}
+
+Describe "Integration Test to ensure PowerShell 7's Native Multi-Line Code Examples render as expected" {
+    It "Mdx file generated for test should exist" -Skip:(-not $isPS7) {
+        $renderedMdxFile | Should -Exist
+    }
     # make sure output is identical
-    It "generates markdown that is identical to the markdown found in our static 'expected' mdx file" {
+    It "Content of generated .mdx is identical to that of expected fixture" -Skip:(-not $isPS7) {
         $renderedMdx | Should -BeExactly $expectedMdx
     }
 
-    It "generates a file without CRLF" {
+    It "Generated .mdx file does not contain CRLF" -Skip:(-not $isPS7) {
         (Get-Content -Path $renderedMdxFile -Raw) -match "`r`n" | Should -Be $False
     }
 }
 
-# -----------------------------------------------------------------------------
-# cleanup
-# -----------------------------------------------------------------------------
-Remove-Item ${global:DocsFolder} -Recurse -Force
+AfterAll {
+    if (Get-Module Alt3.Docusaurus.PowerShell) {
+        Remove-Item $docsFolder -Recurse -Force
+    }
+}
