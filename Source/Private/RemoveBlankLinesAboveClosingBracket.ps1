003b670f0b0057518f12a4f9443d8b31445db942
bravo-kernel <bravo-kernel@users.noreply.github.com>
2022-07-01 19:59:12 +0200

    Rename functions to RemoveBlankLines

--- Source/Private/RemoveBlankLinesAboveClosingBracket.ps1
+++ Source/Private/RemoveBlankLinesAboveClosingBracket.ps1
@@ -1,13 +1,13 @@
-function RemoveEmptyLinesBelowOpeningBracket() {
+function RemoveBlankLinesAboveClosingBracket() {
     <#
         .SYNOPSIS
-            Removes blank lines below lines ending with an opening curly bracket.
+            Removes blank lines below lines ending with a closing curly bracket.
 
         .NOTES
             Required so following steps can trust formatting.
 
         .LINK
-            https://regex101.com/r/LlSt8t/1
+            https://regex101.com/r/xvEd2O/1
     #>
     param(
         [Parameter(Mandatory = $True)][System.IO.FileSystemInfo]$MarkdownFile
@@ -15,13 +15,13 @@ function RemoveEmptyLinesBelowOpeningBracket() {
 
     GetCallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
 
-    Write-Verbose "Removing blank lines below opening curly bracket"
+    Write-Verbose "Removing blank lines above closing curly bracket"
 
     $content = ReadFile -MarkdownFile $MarkdownFile
 
-    $regex = [regex]::new('({\n+\n)')
+    $regex = [regex]::new('(\n\n+\s+}|\n\n})')
 
-    $content = $content -replace $regex, "{`n"
+    $content = $content -replace $regex, "`n}"
 
     # replace file
     WriteFile -MarkdownFile $MarkdownFile -Content $content
