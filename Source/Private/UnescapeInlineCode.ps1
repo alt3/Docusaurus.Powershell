9b60b612969e30f58f1bf8185818865c38d55a7b
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-03-19 16:57:05 +0100

    Adds UnescapeInlineCode

---
+++ Source/Private/UnescapeInlineCode.ps1
@@ -0,0 +1,73 @@
+function UnescapeInlineCode() {
+    <#
+        .SYNOPSIS
+            Unescapes special characters inside single backtick `inline code`.
+
+        .NOTES
+            Please note that platyPS generated `\\\>` is not preserved when importing the
+            file content. This means we have no way to support `\>` inside inline code.
+
+        .LINK
+            https://regex101.com/r/kBl7v8/1
+    #>
+    param(
+        [Parameter(Mandatory = $True)][System.IO.FileSystemInfo]$MarkdownFile
+    )
+
+
+    $content = ReadFile -MarkdownFile $MarkdownFile
+
+    $i = 0
+    [bool]$codeblock = $False
+
+    foreach ($line in $content) {
+        if ($line -match '```' -and $codeblock -eq $False) {
+            $codeblock = $True
+        }
+        elseif ($line -match '```' -and $codeblock -eq $True) {
+            $codeBlock = $False
+        }
+
+        if ($codeblock -eq $True) {
+            $i++
+            continue # do nothing when inside a codeblock
+        }
+
+        $regex = [regex]::new('\`[^`].*?\`')
+
+        if (-not($line -match $regex)) {
+            $i++
+            continue
+        }
+
+        # Process < brackets first
+        $inlineCodes = $regex.Matches($line)
+
+        $inlineCodes  | Foreach-Object {
+            $inlineCode = $_.Value
+
+            $newInlineCode = $inlineCode
+
+            # we first unescape all < brackets
+            if (-not($newInlineCode -match '\\\\\\\<')) {
+                $newInlineCode = $newInlineCode -replace ('\\\<', '<')
+                $line = $line.replace($inlineCode, $newInlineCode)
+            }
+
+            # remaining escaped < must be part of the inline code content (and thus not escaped)
+            if ($newInlineCode -match '\\\\\\\<') {
+                $newInlineCode = $newInlineCode -replace ('\\\\\\\<', '\<KEEP')
+                $line = $line.replace($inlineCode, $newInlineCode)
+            }
+        }
+
+        $line = $line.replace('\<KEEP', '\<')
+
+        $content[$i] = $line.replace('\>', '>') # see .NOTES
+
+        $i++
+    }
+
+    # replace file
+    WriteFile -MarkdownFile $MarkdownFile -Content $content
+}
---
+++ Tests/Integration/UnescapeInlineCode/UnescapeInlineCode.psm1
@@ -0,0 +1,48 @@
+function Test-UnescapeInlineCode {
+    <#
+        .SYNOPSIS
+            Dummy module to ensure proper unescaping of `<angle brackets>` inside inline code.
+
+        .NOTES
+            Replaces platyPS escaped `\<` and `\>` with `<` and `>`.
+
+            Please note that platyPS escaped '&bsol;&bsol;&bsol;&gt;' is not preserved when
+            importing the file content. This means there is no way we can support '&bsol;&gt;'
+            inside inline code.
+
+        .PARAMETER Dummy
+            Dummy `<parameter>` with multiple `<inline code>`.
+
+        .EXAMPLE
+            IMPORTANT: Platy will remove this first-line example code completely if you use a bracket here.
+
+            `<inlinecode>`
+
+            `<inlinecode>` should remain unescaped
+
+            Text with`<inlinecode>`should remain unescaped
+
+            Text with `<inlinecode>` should remain unescaped
+
+            Multiple occurences should all remain unescaped
+
+            `<inlinecode1>``<inlinecode2>`
+
+            `<inlinecode1>` `<inlinecode2>`
+
+            `<inlinecode1>` `<inlinecode2>` with trailing text
+
+            With leading text `<inlinecode1>` `<inlinecode2>`
+
+            With leading text `<inlinecode1>` and `<inlinecode2>`
+
+            With leading text `<inlinecode1>` and `<inlinecode2>` with trailing text
+
+            `<inlinecode1>` `<inlinecode2>` `<inlinecode3>` `<inlinecode4>`
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false)]
+        [string] $Dummy
+    )
+}
