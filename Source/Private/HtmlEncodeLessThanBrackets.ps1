a899e306156fd455193554e32fe06937c09c3ae5
bravo-kernel <bravo-kernel@users.noreply.github.com>
2023-03-19 16:53:56 +0100

    HtmlEncodeLessThanBrackets (except inside code blocks)

---
+++ Source/Private/HtmlEncodeLessThanBrackets.ps1
@@ -0,0 +1,39 @@
+function HtmlEncodeLessThanBrackets() {
+    <#
+        .SYNOPSIS
+            Html encode platyPS generated `\<` brackets (except inside codeblocks).
+
+        .LINK
+            https://regex101.com/r/khoBBE/1
+            https://regex101.com/r/kXRFii/1 C:\>
+    #>
+    param(
+        [Parameter(Mandatory = $True)][System.IO.FileSystemInfo]$MarkdownFile
+    )
+
+    $content = ReadFile -MarkdownFile $MarkdownFile
+
+    $i = 0
+    [bool]$codeblock = $False
+
+    foreach($line in $content) {
+        if ($line -match '```' -and $codeblock -eq $False) {
+            $codeblock = $True
+        } elseif ($line -match '```' -and $codeblock -eq $True) {
+            $codeBlock = $False
+        }
+
+        if ($codeblock -eq $False) {
+            $content[$i] = [regex]::replace($line, '(\\\\\\\<|\\\<)', '&lt;')
+        }
+
+        if ($codeblock -eq $True) {
+            $content[$i] = [regex]::replace($line, '(\\\\\\\<|\\\<)', '<')
+        }
+
+        $i++
+    }
+
+    # replace file
+    WriteFile -MarkdownFile $MarkdownFile -Content $content
+}
---
+++ Tests/Integration/HtmlEncodeLessThanBrackets/HtmlEncodeLessThanBrackets.psm1
@@ -0,0 +1,41 @@
+function Test-HtmlEncodeLessThanBrackets {
+    <#
+        .SYNOPSIS
+            Dummy module to ensure proper html encoding of platyPS escaped opening '<' angle brackets (except inside code blocks).
+
+        .NOTES
+            Invoke-DummyModule -Configuration \<PesterConfiguration\> [\<CommonParameters\>]
+
+        .PARAMETER Dummy
+            Should encode  parameter with 'numbers <A> and B>'
+
+        .EXAMPLE
+            IMPORTANT: Platy will remove this first-line example code completely if you use a bracket here.
+
+            \<
+            \<test should be encoded
+            \< test should be encode
+
+            ```
+            \<
+            \<unencoded
+            \< unencoded
+            \<unencoded\<
+            ```
+
+            encode \<
+            encode\<
+            encode\<this
+
+            ```
+            unencoded \< unencoded
+            ```
+
+            This <example> should also be <encoded>.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false)]
+        [string] $Dummy
+    )
+}
